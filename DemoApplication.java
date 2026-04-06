c2059d6a91314ac34dc3a6b28851ccdbe914a00a
ワンツロン <wangzihrong2025@gmail.com>
2026-04-07 00:04:17 +0900

    Fix package declaration formatting in DemoApplication

--- DemoApplication.java
+++ DemoApplication.java
@@ -1,6 +1,5 @@
 #DemoAPI
-
-  package com.example.demo;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
